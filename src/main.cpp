17efd989d2dfb2d2b4d6b19555769da94378332b
ZazouSam <zazou2000@hotmail.ca>
2023-04-09 19:48:59 -0400

    ajout et modification du main

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include <Arduino.h>
 #include <Wire.h>
 
-//PIN SDA1 = PA18 SCL1 = PA17
-// bq76925 I2C register address macros
+// PIN SDA1 = PA18 SCL1 = PA17
+//  bq76925 I2C register address macros
 #define I2C_GROUP_ADDR 0x04
 // Register addresses 0x00 - 0x05 are Read / Write.
 // Register addresses 0x07 and 0x10 - 0x16 are read only.
@@ -20,27 +20,61 @@
 #define VC4_CAL 0x14
 #define VC5_CAL 0x15
 #define VC6_CAL 0x16
+#define VC_CAL_EXT_1 0x17
+#define VC_CAL_EXT_2 0x18
+#define VREF_CAL_EXT 0x1B
+
+const double Voffset = 0.01;
+const double B_MAX = 16.8;
+const double B_MIN = 12.8;
+const double Gvcout = 0.6;
 
 char ADDRESS;
 float VIOUTsensen;
 
+double GCvc1, GCvc2, GCvc3, GCvc4;
+double OCvc1, OCvc2, OCvc3, OCvc4;
+double GCref;
+
+int VC1_GC_4, VC2_GC_4, VC3_GC_4, VC4_GC_4;
+int VC1_OC_4, VC2_OC_4, VC3_OC_4, VC4_OC_4;
+int VREF_GC_4, VREF_OC_4, VREF_OC_5;
+int VC1_GAIN_CORR, VC2_GAIN_CORR, VC3_GAIN_CORR, VC4_GAIN_CORR;
+int VREF_GAIN_CORR;
+int VC1_OFFSET_CORR, VC2_OFFSET_CORR, VC3_OFFSET_CORR, VC4_OFFSET_CORR;
+int VREF_OFFSET_CORR;
+
+int test;
+
 void setup()
 {
-  // ADC setup pin A7
-  pinMode(A7, INPUT);
-  // ADC setup pin A7
-  pinMode(A6, INPUT);
+  // ADC setup pin A0 Temp
+  pinMode(A0, INPUT);
+  // ADC setup pin A1 Volt
+  pinMode(A1, INPUT);
+  // ADC setup pin A2 courant
+  pinMode(A2, INPUT);
+  analogReadResolution(12); // adc resolution 12 bits
+
   // adc external reference voltage 3.3V
   // put your setup code here, to run once:
   // set up Wire1
   Wire1.begin();
   // set up serial
-  Serial.begin(9600);
+  Serial.begin(115200);
   // set up bq76925
-  Wire1.beginTransmission((I2C_GROUP_ADDR << 3) + CELL_CTL);
-  Wire1.write(0x10);
+  // Wire1.beginTransmission((I2C_GROUP_ADDR << 3) + STATUS);
+  // Wire1.write(0x00);
+  // Wire1.endTransmission();
+  delay(10);
+  Wire1.beginTransmission((I2C_GROUP_ADDR << 3) + POWER_CTL);
+  Wire1.write(0x5C);
   Wire1.endTransmission();
   delay(10);
+  // Wire1.beginTransmission((I2C_GROUP_ADDR << 3) + CELL_CTL);
+  // Wire1.write(0x10);
+  // Wire1.endTransmission();
+  // delay(10);
 
   // Wire1.beginTransmission((I2C_GROUP_ADDR << 3) + CONFIG_1);
   // Wire1.write(byte(0xE0));
@@ -51,176 +85,191 @@ void setup()
   Wire1.write(0x00);
   Wire1.endTransmission();
   delay(5000);
-  int adc = analogRead(A6);
+  int adc = analogRead(A2);
   Serial.print("ADC: ");
   Serial.print(adc);
-  VIOUTsensen = adc * (3.3 / 1023.0);
+  VIOUTsensen = adc * (3.3 / 4096.0);
   Serial.print("  VIOUTsensen: ");
   Serial.print(VIOUTsensen);
 
   delay(50);
 
   Wire1.beginTransmission((I2C_GROUP_ADDR << 3) + CONFIG_2);
-  Wire1.write(0x11);
+  Wire1.write(0x01);
   Wire1.endTransmission();
   delay(10);
 
-  Wire1.beginTransmission((I2C_GROUP_ADDR << 3) + POWER_CTL);
-  Wire1.write(B01011101);
-  Wire1.endTransmission();
-  delay(10);
-  int test;
   Wire1.requestFrom((I2C_GROUP_ADDR << 3) + CONFIG_1, 1); // request 6 bytes from slave device #8
   while (Wire1.available())
-  {                     // slave may send less than requested
+  {                      // slave may send less than requested
     test = Wire1.read(); // receive a byte as character
   }
 
-  delay(50);
+  delay(500);
   while (test != 0x00)
   {
     Wire1.requestFrom((I2C_GROUP_ADDR << 3) + CONFIG_1, 1); // request 6 bytes from slave device #8
     while (Wire1.available())
-    {                     // slave may send less than requested
+    {                      // slave may send less than requested
       test = Wire1.read(); // receive a byte as character
     }
-    delay(50);
+    delay(500);
     Serial.print("test: ");
     Serial.print(test);
     Serial.println("  rip");
   }
-  
+
   Wire1.beginTransmission((I2C_GROUP_ADDR << 3) + CONFIG_1);
   Wire1.write(0x05);
   Wire1.endTransmission();
   delay(10);
+
+
+  Wire1.requestFrom((I2C_GROUP_ADDR << 3) + VREF_CAL, 1);
+  while (Wire1.available())
+  {
+    int temp = Wire1.read();
+    VREF_OFFSET_CORR = temp >> 4;
+    VREF_GAIN_CORR = temp & 0x0F;
+  }
+  delay(10);
+  Wire1.requestFrom((I2C_GROUP_ADDR << 3) + VC1_CAL, 1);
+  while (Wire1.available())
+  {
+    int temp = Wire1.read();
+    VC1_OFFSET_CORR = temp >> 4;
+    VC1_GAIN_CORR = temp & 0x0F;
+  }
+  delay(10);
+  Wire1.requestFrom((I2C_GROUP_ADDR << 3) + VC2_CAL, 1);
+  while (Wire1.available())
+  {
+    int temp = Wire1.read();
+    VC2_OFFSET_CORR = temp >> 4;
+    VC2_GAIN_CORR = temp & 0x0F;
+  }
+  delay(10);
+  Wire1.requestFrom((I2C_GROUP_ADDR << 3) + VC3_CAL, 1);
+  while (Wire1.available())
+  {
+    int temp = Wire1.read();
+    VC3_OFFSET_CORR = temp >> 4;
+    VC3_GAIN_CORR = temp & 0x0F;
+  }
+  delay(10);
+  Wire1.requestFrom((I2C_GROUP_ADDR << 3) + VC4_CAL, 1);
+  while (Wire1.available())
+  {
+    int temp = Wire1.read();
+    VC4_OFFSET_CORR = temp >> 4;
+    VC4_GAIN_CORR = temp & 0x0F;
+  }
+  delay(10);
+  Wire1.requestFrom((I2C_GROUP_ADDR << 3) + VC_CAL_EXT_1, 1);
+  while (Wire1.available())
+  {
+    int temp = Wire1.read();
+    VC1_OC_4 = temp & B10000000;
+    VC1_GC_4 = temp & B01000000;
+    VC2_OC_4 = temp & B00100000;
+    VC2_GC_4 = temp & B00010000;
+  }
+  delay(10);
+  Wire1.requestFrom((I2C_GROUP_ADDR << 3) + VC_CAL_EXT_2, 1);
+  while (Wire1.available())
+  {
+    int temp = Wire1.read();
+    VC3_OC_4 = temp & B10000000;
+    VC3_GC_4 = temp & B01000000;
+    VC4_OC_4 = temp & B00100000;
+    VC4_GC_4 = temp & B00010000;
+  }
+  delay(10);
+  Wire1.requestFrom((I2C_GROUP_ADDR << 3) + VREF_CAL_EXT, 1);
+  while (Wire1.available())
+  {
+    int temp = Wire1.read();
+    VREF_OC_5 = temp & B00000100;
+    VREF_OC_4 = temp & B00000010;
+    VREF_GC_4 = temp & B00000001;
+  }
+  delay(10);
+  GCref = (1 + ((VREF_GC_4 << 4)+ VREF_GAIN_CORR)* 0.001) + (((VREF_OC_5 << 5) + (VREF_OC_4 << 4) + VREF_OFFSET_CORR) * 0.001)/3;
+  GCvc1 = ((VC1_GC_4 << 4) + VC1_GAIN_CORR) * 0.001;
+  OCvc1 = ((VC1_OC_4 << 4) + VC1_OFFSET_CORR) * 0.001;
+  GCvc2 = ((VC2_GC_4 << 4) + VC2_GAIN_CORR) * 0.001;
+  OCvc2 = ((VC2_OC_4 << 4) + VC2_OFFSET_CORR) * 0.001;
+  GCvc3 = ((VC3_GC_4 << 4) + VC3_GAIN_CORR) * 0.001;
+  OCvc3 = ((VC3_OC_4 << 4) + VC3_OFFSET_CORR) * 0.001;
+  GCvc4 = ((VC4_GC_4 << 4) + VC4_GAIN_CORR) * 0.001;
+  OCvc4 = ((VC4_OC_4 << 4) + VC4_OFFSET_CORR) * 0.001;
 }
 
 void loop()
 {
-  // adc read and print to serial
+  double VCOUT;
+  double Isense;
+  double Rsense = 0.001;
+  double VC1, VC2, VC3, VC4;
+  double Vref;
 
-  // convert to voltage
-  float VCOUT;
-  float VIOUTsensep;
-  float Vsense;
-  float Isense;
-  float Rsense = 0.001;
-  float VC1, VC2, VC3, VC4;
-  float VC1cal, VC2cal, VC3cal, VC4cal;
-  float Vref;
-  float GCvc1, GCvc2, GCvc3, GCvc4;
-  float OCvc1, OCvc2, OCvc3, OCvc4;
-  float GCref;
-  float VCtotal;
-
+  double VCtotal;
+  uint32_t adc;
+  double pourcentage_batterie;
   // Serial.println(VCOUT);
   // read from bq76925 on ADDRESS
   int gain;
-  Wire1.requestFrom((I2C_GROUP_ADDR << 3) + CONFIG_1, 1); // request 6 bytes from slave device #8
-  while (Wire1.available())
-  {              // slave may send less than requested
-    gain = Wire1.read(); // receive a byte as character
-  }
-
-  delay(50);
-  /*
-    Wire1.requestFrom((I2C_GROUP_ADDR << 3) + VREF_CAL, 1); // request 6 bytes from slave device #8
-    while (Wire1.available())
-    {                     // slave may send less than requested
-      Vref = Wire1.read(); // receive a byte as character
-    }
-    delay(50);
-
-    Wire1.requestFrom((I2C_GROUP_ADDR << 3) + POWER_CTL, 1); // request 6 bytes from slave device #8
-    while (Wire1.available())
-    {              // slave may send less than requested
-      Wire1.read(); // receive a byte as character
-    }
-    delay(50);
-
-    Wire1.beginTransmission((I2C_GROUP_ADDR << 3) + CELL_CTL);
-    Wire1.write(0x10);
-    Wire1.endTransmission();
-    Wire1.requestFrom((I2C_GROUP_ADDR << 3) + CELL_CTL, 1); // request 6 bytes from slave device #8
-    while (Wire1.available())
-    {              // slave may send less than requested
-      Wire1.read(); // receive a byte as character
-    }
-  */
-
-  delay(100);
-
-  int adc = analogRead(A6);
-  Serial.print("ADC: ");
-  Serial.print(adc);
-  VIOUTsensep = adc * (3.3 / 1023.0);
-  Serial.print("  VIOUTsensep: ");
-  Serial.print(VIOUTsensep);
-  if (gain == 0x00)
-  {
-    Vsense = -(VIOUTsensep - VIOUTsensen) / 4.0;
-  }
-  else if (gain == 0x04)
-  {
-    Vsense = -(VIOUTsensep - VIOUTsensen) / 8.0;
-  }
-  
-  Serial.print("  Vsense: ");
-  Serial.print(Vsense);
-  Isense = Vsense / Rsense;
-  Serial.print("  Isense: ");
-  Serial.println(Isense);
-
-  delay(50);
-  /*
+  delay(250);
   Wire1.beginTransmission((I2C_GROUP_ADDR << 3) + CELL_CTL);
   Wire1.write(0x10);
   Wire1.endTransmission();
   delay(10);
-  adc = analogRead(A7);
+  adc = analogRead(A2);
 
-  VCOUT = adc * (3.3 / 1023.0);
-  VC1 = (VCOUT / 0.6);
+  VCOUT = adc * (3.3 / 4096.0) - Voffset;
+  VC1 = ((VCOUT * GCref + OCvc1)/Gvcout)* (1 + GCvc1);
   Serial.print("VC1: ");
-  Serial.println(VC1);
+  Serial.print(VC1);
 
   Wire1.beginTransmission((I2C_GROUP_ADDR << 3) + CELL_CTL);
   Wire1.write(0x11);
   Wire1.endTransmission();
   delay(10);
 
-  adc = analogRead(A7);
-  VCOUT = adc * (3.3 / 1023.0);
-  VC2 = (VCOUT / 0.6);
-  Serial.print("VC2: ");
-  Serial.println(VC2);
+  adc = analogRead(A2);
+  VCOUT = adc * (3.3 / 4096.0) - Voffset;
+  VC2 = ((VCOUT * GCref + OCvc2)/Gvcout)* (1 + GCvc2);
+  Serial.print(" VC2: ");
+  Serial.print(VC2);
   // do the same thing for VC3, VC4, VC5, VC6
   Wire1.beginTransmission((I2C_GROUP_ADDR << 3) + CELL_CTL);
   Wire1.write(0x12);
   Wire1.endTransmission();
   delay(10);
 
-  adc = analogRead(A7);
-  VCOUT = adc * (3.3 / 1023.0);
-  VC3 = (VCOUT / 0.6);
-  Serial.print("VC3: ");
-  Serial.println(VC3);
+  adc = analogRead(A2);
+  VCOUT = adc * (3.3 / 4096.0) - Voffset;
+  VC3 = ((VCOUT * GCref + OCvc3)/Gvcout)* (1 + GCvc3);
+  Serial.print(" VC3: ");
+  Serial.print(VC3);
 
   Wire1.beginTransmission((I2C_GROUP_ADDR << 3) + CELL_CTL);
   Wire1.write(0x13);
   Wire1.endTransmission();
   delay(10);
 
-  adc = analogRead(A7);
-  VCOUT = adc * (3.3 / 1023.0);
-  VC4 = (VCOUT / 0.6);
-  Serial.print("VC4: ");
-  Serial.println(VC4);
+  adc = analogRead(A2);
+  VCOUT = adc * (3.3 / 4096.0) - Voffset;
+  VC4 = ((VCOUT * GCref + OCvc4)/Gvcout)* (1 + GCvc4);
+  Serial.print(" VC4: ");
+  Serial.print(VC4);
+  delay(10);
 
-  Serial.print("Vctotal: ");
+  Serial.print(" Vctotal: ");
   VCtotal = VC1 + VC2 + VC3 + VC4;
-  Serial.println(VCtotal);
-  */
-  delay(500);
+  Serial.print(VCtotal);
+
+  pourcentage_batterie = (VCtotal - B_MIN) / (B_MAX - B_MIN) * 100;
+  Serial.print(" pourcentage batterie: ");
+  Serial.println(pourcentage_batterie);
 }
